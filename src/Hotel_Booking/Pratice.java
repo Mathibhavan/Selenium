9b470b14df45e389ebb4f0ed1821f26c9e74578b
721901 <721901@PCIN455916.cts.com>
2021-04-20 11:22:19 +0530

    Second commit

---
+++ src/Hotel_Booking/Pratice.java
@@ -0,0 +1,21 @@
+package Hotel_Booking;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+
+public class Pratice extends Base_Class{
+
+
+	
+
+	public static void main(String[] args) throws IOException {
+		
+		driver=BrowserLaunch("Chrome");
+		getURL("https://www.w3schools.com/java/java_constructors.asp");
+		SnapShot();
+
+		
+	}
+
+}
